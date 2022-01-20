fb935bbdac80babff6c67e485284614ef962c690
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2022-01-20 11:33:23 +0800

    added boilerplate

---
+++ dbs-engagement/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+const App = () => {
+    return (
+          <div>
+            <BrowserRouter>
+                    
+            </BrowserRouter>
+        </div>
+          
+    );
+};
+
+export default App;
